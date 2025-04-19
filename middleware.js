1379dfe905f37ff9698ec197ad25a96d63647e56
Alex <alexbhusal@icloud.com>
2025-04-19 22:56:13 +0545

    Route Protected

--- middleware.js
+++ middleware.js
@@ -1,16 +1,17 @@
-import { NextResponse } from 'next/server';
+import { NextResponse,NextRequest } from 'next/server';
 
-const protectedRoutes = ['/dashboard'];
 export function middleware(req) {
-    const url = req.nextUrl.clone();
-    const token = req.cookies.get('token')?.value;
-  
-    if (protectedRoutes.includes(url.pathname)) {
-      if (!token) {
-        url.pathname = '/login';
-        return NextResponse.redirect(url);
-      }
-    }
-  
-    return NextResponse.next();
+  const url = req.nextUrl.clone();
+  const token = req.cookies.get('token')?.value;
+
+  if (!token) {
+    url.pathname = '/login';
+    return NextResponse.redirect(url);
   }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/dashboard/:path*'],
+};
